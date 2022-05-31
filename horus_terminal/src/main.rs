6855a29b12e4127f589eb6a17a5cfea126c89295
Finn Fiedler <finn.fiedler@smapiot.com>
2022-05-31 20:38:16 +0200

    nightly backup

--- horus_terminal/src/main.rs
+++ horus_terminal/src/main.rs
@@ -1,50 +1,124 @@
 use yew::prelude::*;
+use gloo::console::{self, Timer};
+use gloo::timers::callback::{Interval, Timeout};
 
-enum Msg {
-    AddOne,
+struct Position {
+    agent_id: &'static str,
+    // exchange_name: CompactString,
+    // quote_name: CompactString,
+    // currency: CompactString,
+    // date_of_buy: CompactString,
+    // date_of_sell: CompactString,
+    // buy_price: f32,
+    // sell_price: f32,
+    // amount: f32,
 }
 
-struct Model {
-    value: i64,
+impl Clone for Position {
+    fn clone(&self) -> Self {
+        Self { agent_id: self.agent_id.clone() }
+    }
 }
 
-impl Component for Model {
-    type Message = Msg;
+impl Copy for Position {
+    
+}
+
+struct PositionTable {
+    positions: [Position; 2],
+    publish_task: Interval
+}
+
+impl Component for PositionTable {
+    type Message = Position;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
+    fn create(ctx: &Context<Self>) -> Self {
+
+        let publish_interval = {
+            let link = ctx.link().clone();
+
+            let fake_position = Position {
+                agent_id: "#A337",
+                // exchange_name: CompactString::from("BINANCE"),
+                // quote_name: CompactString::from("BTC"),
+                // currency: CompactString::from("EUR"),
+                // date_of_buy: CompactString::from("DROELF"),
+                // date_of_sell: CompactString::from("DROELF"),
+                // buy_price: 13.98,
+                // sell_price: 29.45,
+                // amount: 12.
+            };
+            Interval::new(1000, move || link.send_message(fake_position))
+        };
+
         Self {
-            value: 0,
+            positions: [
+                Position {
+                    agent_id: "#A337",
+                    // exchange_name: CompactString::from("BINANCE"),
+                    // quote_name: CompactString::from("BTC"),
+                    // currency: CompactString::from("EUR"),
+                    // date_of_buy: CompactString::from("DROELF"),
+                    // date_of_sell: CompactString::from("DROELF"),
+                    // buy_price: 13.98,
+                    // sell_price: 29.45,
+                    // amount: 12.
+                },
+                Position {
+                    agent_id: "#A337",
+                    // exchange_name: CompactString::from("BINANCE"),
+                    // quote_name: CompactString::from("IOTA"),
+                    // currency: CompactString::from("EUR"),
+                    // date_of_buy: CompactString::from("DROELF"),
+                    // date_of_sell: CompactString::from("DROELF"),
+                    // buy_price: 53.98,
+                    // sell_price: 59.45,
+                    // amount: 16.
+                }
+            ],
+            publish_task: publish_interval,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
+        true
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <table>
+              <tr>
+                <th>{ "Agent" }</th>
+                <th>{ "Exchange" }</th>
+                <th>{ "Quote" }</th>
+                <th>{ "Buy" }</th>
+                <th>{ "Sell" }</th>
+                <th>{ "Absolute Return" }</th>
+                <th>{ "Relative Return" }</th>
+                <th>{ "Holding Time" }</th>
+              </tr>
+              {
+                self.positions.iter().map(|pos| {
+                  html!{
+                    <tr>
+                      <td>{ pos.agent_id }</td>
+                    //   <td>{ pos.exchange_name }</td>
+                    //   <td>{ pos.agent_id }</td>
+                    //   <td>{ pos.exchange_name }</td>
+                    //   <td>{ pos.agent_id }</td>
+                    //   <td>{ pos.exchange_name }</td>
+                    //   <td>{ pos.agent_id }</td>
+                    //   <td>{ pos.exchange_name }</td>
+                    </tr>
+                  }
+                }).collect::<Html>()
+              }
+            </table> 
         }
     }
-}
-
-impl Model {
-    
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<PositionTable>();
 }
