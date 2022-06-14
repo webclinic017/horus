1d808ef08dd95bdcd4db583f189a59e49fc4c0c7
Finn Fiedler <finn.fiedler@smapiot.com>
2022-06-14 22:04:15 +0200

    daily backup

--- horus_exchanges/src/mock_exchange/mock_market_connector.rs
+++ horus_exchanges/src/mock_exchange/mock_market_connector.rs
@@ -1,12 +1,14 @@
-use horus_finance::{aggregate::Aggregate, market_snapshot::MarketSnapshot, order::Order};
+use std::cell::RefCell;
+
+use horus_finance::{aggregate::Aggregate, market_snapshot::MarketSnapshot, order::Order, order_side::OrderSide};
 
 use crate::connectors::market_connector::MarketConnector;
 
 pub struct MockMarketConnector {
     pub current_ask: f32,
     pub current_bid: f32,
-    pub asset_balance: usize,
-    pub cash_balance: f32
+    pub asset_balance: RefCell<usize>,
+    pub cash_balance: RefCell<f32>
 }
 
 impl MockMarketConnector {
@@ -14,8 +16,8 @@ impl MockMarketConnector {
         MockMarketConnector {
             current_ask: 0.,
             current_bid: 0.,
-            asset_balance: 0,
-            cash_balance: initial_cash_balance
+            asset_balance: RefCell::new(0),
+            cash_balance: RefCell::new(initial_cash_balance)
         }
     }
 
@@ -25,33 +27,109 @@ impl MockMarketConnector {
     }
 
     pub fn inject_aggregate(&mut self, order: Aggregate) {
-        todo!();
+        self.current_bid = order.close;
     }
 
     pub fn get_asset_balance(&self) -> usize {
-        self.asset_balance
+        *self.asset_balance.borrow()
     }
 
     pub fn get_cash_balance(&self) -> f32 {
-        self.cash_balance
+        *self.cash_balance.borrow()
     }
 }
 
 impl MarketConnector for MockMarketConnector {
 
     fn route_make_order(&self, order: &Order) -> bool {
-        todo!()
+        
+        let mut cash_balance_ref = self.cash_balance.borrow_mut();
+        let mut asset_balance_ref = self.asset_balance.borrow_mut();
+
+        let price;
+        let cash_to_transfer;
+        let lot_to_transfer;
+        match order.side {
+            OrderSide::BUY => {
+                price = order.price.unwrap();
+                cash_to_transfer = order.quantity as f32 * price;
+
+                if cash_to_transfer > *cash_balance_ref {
+                    return false
+                }
+
+                lot_to_transfer = (cash_to_transfer / price).floor();
+
+                *asset_balance_ref += lot_to_transfer as usize;
+                *cash_balance_ref -= cash_to_transfer;
+                
+            },
+            OrderSide::SELL => {
+
+                if order.quantity > *asset_balance_ref {
+                    return false;
+                }
+
+                price = order.price.unwrap();
+                cash_to_transfer = order.quantity as f32 * price;
+
+                lot_to_transfer = (cash_to_transfer / price).floor();
+
+                *asset_balance_ref -= lot_to_transfer as usize;
+                *cash_balance_ref += cash_to_transfer;
+            }
+        }
+
+        true
     }
 
     fn route_take_order(&self, order: &Order) -> bool {
-        todo!()
+        
+        let mut cash_balance_ref = self.cash_balance.borrow_mut();
+        let mut asset_balance_ref = self.asset_balance.borrow_mut();
+
+        let price;
+        let cash_to_transfer;
+        let lot_to_transfer;
+        match order.side {
+            OrderSide::BUY => {
+                price = self.current_ask;
+                cash_to_transfer = order.quantity as f32 * price;
+
+                if cash_to_transfer > *cash_balance_ref {
+                    return false
+                }
+
+                lot_to_transfer = (cash_to_transfer / price).floor();
+
+                *asset_balance_ref += lot_to_transfer as usize;
+                *cash_balance_ref -= cash_to_transfer;
+                
+            },
+            OrderSide::SELL => {
+
+                if order.quantity > *asset_balance_ref {
+                    return false;
+                }
+
+                price = self.current_bid;
+                cash_to_transfer = order.quantity as f32 * price;
+
+                lot_to_transfer = (cash_to_transfer / price).floor();
+
+                *asset_balance_ref -= lot_to_transfer as usize;
+                *cash_balance_ref += cash_to_transfer;
+            }
+        }
+
+        true
     }
 
-    fn get_historical_snapshots(&self, start: chrono::DateTime<chrono::Utc>, end: chrono::DateTime<chrono::Utc>) -> Vec<MarketSnapshot> {
+    fn get_historical_snapshots(&self, _start: chrono::DateTime<chrono::Utc>, _end: chrono::DateTime<chrono::Utc>) -> Vec<MarketSnapshot> {
         panic!("This connector is only used for backtesting and can not provide historical data")
     }
 
-    fn get_historical_aggregates(&self, start: chrono::DateTime<chrono::Utc>, end: chrono::DateTime<chrono::Utc>) -> Vec<Aggregate> {
+    fn get_historical_aggregates(&self, _start: chrono::DateTime<chrono::Utc>, _end: chrono::DateTime<chrono::Utc>) -> Vec<Aggregate> {
         panic!("This connector is only used for backtesting and can not provide historical data")
     }
 }
--- horus_strategies/src/strategies/inter_market_arbitrage.rs
+++ horus_strategies/src/strategies/inter_market_arbitrage.rs
@@ -1,24 +1,24 @@
-use horus_data_streams::receivers::data_receiver::DataReceiver;
-use horus_exchanges::connectors::market_connector::MarketConnector;
-use horus_finance::aggregate::Aggregate;
+// use horus_data_streams::receivers::data_receiver::DataReceiver;
+// use horus_exchanges::connectors::market_connector::MarketConnector;
+// use horus_finance::aggregate::Aggregate;
 
-use super::strategy::Strategy;
+// use super::strategy::Strategy;
 
-struct InterMarketArbitrage<'a, ExOne: MarketConnector, ExTwo: MarketConnector> {
-    exchange_connector_01: &'a ExOne,
-    exchange_connector_02: &'a ExTwo
-}
+// struct InterMarketArbitrage<'a, ExOne: MarketConnector, ExTwo: MarketConnector> {
+//     exchange_connector_01: &'a ExOne,
+//     exchange_connector_02: &'a ExTwo
+// }
 
-impl<'a, ExOne: MarketConnector, ExTwo: MarketConnector> Strategy for InterMarketArbitrage<'a, ExOne, ExTwo> {
-    fn run(&self) -> std::thread::JoinHandle<()> {
-        todo!()
-    }
+// impl<'a, ExOne: MarketConnector, ExTwo: MarketConnector> Strategy for InterMarketArbitrage<'a, ExOne, ExTwo> {
+//     fn run(&self) -> std::thread::JoinHandle<()> {
+//         todo!()
+//     }
 
-    fn get_market_connectors(&self) -> &Vec<&dyn MarketConnector> {
-        todo!()
-    }
+//     fn get_market_connectors(&self) -> &Vec<&dyn MarketConnector> {
+//         todo!()
+//     }
 
-    fn get_name() -> &'static str {
-        return "Inter Exchange Arbitrage";
-    }
-}
+//     fn get_name() -> &'static str {
+//         return "Inter Exchange Arbitrage";
+//     }
+// }
