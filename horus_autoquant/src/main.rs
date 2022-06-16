356135413826efa899d81978a4807aa25c4154b1
Finn <finn.f@mailbox.org>
2022-06-16 22:21:20 +0200

    daily backup

--- horus_autoquant/src/main.rs
+++ horus_autoquant/src/main.rs
@@ -1,17 +1,23 @@
 use chrono::TimeZone;
-use horus_backtesting::{run_backtest_on_aggregates, markets_event_simulation::{MarketsSnapshotEventSimulation, MarketsAggregateEventSimulation}, test_market_adapter::TestMarketAdapter};
+use horus_backtesting::{run_backtest_on_aggregates, markets_event_simulation::{MarketsSnapshotEventSimulation, MarketsAggregateEventSimulation}, test_market_adapter::TestMarketAdapter, run_backtest_on_snapshots};
 use horus_data_streams::receivers::{binance_market_data_receiver::BinanceSpotAggregateReceiver, data_receiver::DataReceiver};
+use horus_exchanges::mock_exchange::mock_market_connector::MockMarketConnector;
 use horus_finance::aggregate::Aggregate;
 use horus_strategies::strategies::{inter_market_arbitrage::InterMarketArbitrageStrategy, buy_low_sell_high::BuyLowSellHighStrategy};
+
+fn test_twitter_strategy() {
+    
+}
 
 fn test_inter_market_arbitrage_strategy() {
 
     //1. Describe Strategy
     let binance_spot = BinanceSpotAggregateReceiver::new("BTCEUR".to_string(), "5m".to_string(), &|_a| {});
     let bitfinex_spot = BitfinexSpotMarketConnector::new("BTCEUR".to_string(), "5m".to_string(), &|_a| {});
-    let fake_market_01 = TestMarketAdapter::new();
-    let fake_market_02 = TestMarketAdapter::new();
-    let strategy = InterMarketArbitrageStrategy::<TestMarketAdapter, TestMarketAdapter>::new(&fake_market_01, &fake_market_02);
+    let mock_market_01 = MockMarketConnector::new(1000.);
+    let mock_market_02 = MockMarketConnector::new(0.);
+    let test_adapter = TestMarketAdapter::new();
+    let strategy = InterMarketArbitrageStrategy::<MockMarketConnector, MockMarketConnector>::new(&mock_market_01, &mock_market_02);
 
     //2. Describe Simulation
     let start_date = chrono::Utc.ymd(2015, 1, 1).and_hms(0, 0, 0);
@@ -19,7 +25,7 @@ fn test_inter_market_arbitrage_strategy() {
     let historical = binance_spot.get_historical_snapshots(start_date, end_date);
     let simulation = MarketsSnapshotEventSimulation::new();
     simulation.insert("market_01", historical);
-    let _result = run_backtest_on_aggregates(&strategy, &historical, &fake_market);
+    let _result = run_backtest_on_snapshots(&strategy, simulation, &test_adapter);
 
     //3. Report
     todo!()
@@ -29,8 +35,10 @@ fn test_blsh_strategy() {
 
     //1. Describe Strategy
     let binance_spot = BinanceSpotAggregateReceiver::new("BTCEUR".to_string(), "5m".to_string(), &|_a| {});
-    let fake_market = TestMarketAdapter::new();
-    let strategy = BuyLowSellHighStrategy::<TestMarketAdapter>::new(&fake_market);
+    let mock_market = MockMarketConnector::new(1000.);
+    let test_adapter = TestMarketAdapter::new();
+    test_adapter.add_market_connector("market_01", &mock_market);
+    let strategy = BuyLowSellHighStrategy::<MockMarketConnector>::new(&mock_market);
 
     //2. Describe Simulation
     let start_date = chrono::Utc.ymd(2015, 1, 1).and_hms(0, 0, 0);
@@ -38,7 +46,7 @@ fn test_blsh_strategy() {
     let historical = binance_spot.get_historical_data(start_date, end_date);
     let simulation = MarketsAggregateEventSimulation::new();
     simulation.insert("market_01", historical);
-    let _result = run_backtest_on_aggregates(&strategy, simulation, &fake_market);
+    let _result = run_backtest_on_aggregates(&strategy, simulation, &test_adapter);
 
     //3. Report
     todo!()
--- horus_backtesting/src/lib.rs
+++ horus_backtesting/src/lib.rs
@@ -4,36 +4,6 @@ use market_events::MarketNewAggregateEvent;
 use markets_event_simulation::{MarketsAggregateEventSimulation, MarketsSnapshotEventSimulation};
 use test_market_adapter::TestMarketAdapter;
 
-// pub struct MarketSimulation {
-//     index: usize,
-//     pub market_data: Vec<Aggregate>
-// }
-
-// impl MarketSimulation {
-
-//     fn get_current_bid(&self) -> f32 {
-//         self.market_data[self.index].close
-//     }
-
-//     fn get_current_ask(&self) -> f32 {
-//         self.market_data[self.index].close
-//     }
-// }
-
-// impl Iterator for MarketSimulation {
-//     type Item = Aggregate;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-
-//         if self.market_data.len() > self.index {
-//             self.index += 1;
-//             Some(self.market_data[self.index - 1])
-//         } else {
-//             None
-//         }
-//     }
-// }
-
 pub struct BacktestResult {
     pub profit_loss_rel: f32,
     pub profit_loss_abs: f32,
@@ -60,15 +30,36 @@ impl PartialOrd for BacktestResult {
     }
 }
 
-// impl Clone for BacktestResult {
-//     fn clone(&self) -> Self {
-//         Self { profit_loss_rel: self.profit_loss_rel, profit_loss_abs: self.profit_loss_abs, alpha: self.alpha }
-//     }
-// }
+pub fn run_backtest_on_snapshots<STRATEGY: Strategy>(strategy: &STRATEGY, test_simulation: MarketsSnapshotEventSimulation, adapter: &TestMarketAdapter) -> BacktestResult {
 
-// impl Copy for BacktestResult { }
+    // SETUP
+    adapter.set_initial_state();
 
-/// The mock market has to be connected to the strategy
+    let strategy_handle = strategy.run();
+
+    // RUN TESTS
+    for event in test_simulation {
+        adapter.inject_snapshot(&event.market_key, event.snapshot);
+    }
+
+    strategy_handle.join().unwrap();
+
+    // END
+    adapter.finalize_positions();
+
+    let buy_and_hold_rel: f32 = adapter.get_buy_and_hold_relative();
+    let strategy_rel: f32 = adapter.get_strategy_relative();
+    let strategy_abs: f32 = adapter.get_strategy_absolute();
+
+    let alpha: f32 = strategy_rel / buy_and_hold_rel - 1.;
+
+    BacktestResult { 
+        profit_loss_rel: strategy_rel, 
+        profit_loss_abs: strategy_abs,
+        alpha
+    }
+}
+
 pub fn run_backtest_on_aggregates<STRATEGY: Strategy>(strategy: &STRATEGY, test_simulation: MarketsAggregateEventSimulation, adapter: &TestMarketAdapter) -> BacktestResult {
 
     // SETUP
