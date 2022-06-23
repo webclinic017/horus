af5d4ebc716cd817035fc801bbd9c79f5a9fd9b9
Finn Fiedler <finn.fiedler@smapiot.com>
2022-06-23 14:50:57 +0200

    compiling backup

--- horus_backtesting/src/backtest_simulation.rs
+++ horus_backtesting/src/backtest_simulation.rs
@@ -1,13 +1,13 @@
 use horus_finance::time_series_element::TimeSeriesElement;
 
-use crate::{simulation_element::SimulationElement, test_market_adapter::TestMarketAdapter};
+use crate::simulation_element::SimulationElement;
 
 pub struct BacktestSimulation<'a> {
     time_series_elements: Vec<SimulationElement<'a, Box<dyn TimeSeriesElement>>>,
 }
 
 impl<'a> BacktestSimulation<'a> {
-    pub fn run(&mut self, test_adapter: &TestMarketAdapter) {
+    pub fn run(&mut self) {
         for element in self.time_series_elements.drain(..) {
             element.stream.inject(element.datum);
         }
--- horus_backtesting/src/simulation_element.rs
+++ horus_backtesting/src/simulation_element.rs
@@ -3,5 +3,6 @@ use horus_finance::time_series_element::TimeSeriesElement;
 
 pub struct SimulationElement<'a, DATATYPE> {
     pub datum: DATATYPE,
-    pub stream: &'a MockDataStream<Box<dyn TimeSeriesElement>, 2>
+    pub stream: &'a MockDataStream<Box<dyn TimeSeriesElement>, 2>,
+    pub publish_to_mock_exchange: Option<&'a dyn Fn()>
 }
