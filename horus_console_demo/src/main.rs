cc7b8c2a2210d7daa07d2e25fbe6ba72827e9f44
Finn Fiedler <finn.fiedler@smapiot.com>
2022-07-21 15:47:30 +0200

    daily backup

--- horus_console_demo/src/main.rs
+++ horus_console_demo/src/main.rs
@@ -5,16 +5,22 @@ use horus_reporters::{console_reporter::ConsoleReporter, reporter::Reporter};
 use horus_strategies::strategies::monte_carlo::MonteCarloAggregates;
 
 fn main() {
-    let mut market_connector = MockMarketConnector::new(1000.);
+
+    let mut binance = BinanceFuturesAggregateStream::new("BTCEUR".to_string(), "1m".to_string());
+    let mut market_connector = MockMarketConnector::new(10000.);
     market_connector.set_fake_exchange_name("BINANCE FUTURES");
     market_connector.set_fake_market_name("BTC/EUR");
     market_connector.set_fake_currency_symbol("€");
+
     let mut reporter = ConsoleReporter::new(&market_connector.get_exchange_name(), &market_connector.get_market_name(), &market_connector.get_currency_symbol());
     reporter.update_status(true);
+
     let mut strategy = MonteCarloAggregates::new(market_connector, reporter);
+
     let mut on_data = |aggregate: Aggregate| { 
         strategy.next(aggregate); 
     };
-    let mut binance = BinanceFuturesAggregateStream::new(&mut on_data, "BTCEUR".to_string(), "1m".to_string());
-    binance.start_listening();
+    
+    binance.start_listening(&mut on_data);
+    binance.start_with_replay()
 }
